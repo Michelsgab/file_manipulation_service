090c32e9803a2aef58361f42659f75d8afc10430
Thiago Mussolini <mussolinithiago1994@gmail.com>
2022-08-16 15:36:37 -0300

    Adicionado metodo de salvar do curriculo

--- service/ProjetoService.py
+++ service/ProjetoService.py
@@ -16,12 +16,19 @@ class ProjetoService:
         with open(f'imagens/{projeto.nome}.png', 'wb') as imagem_nova:
             imagem_nova.write(base64.decodebytes(bytes(projeto.foto, 'UTF8')))
         projeto.foto = f"Imagem do {projeto.nome}"
+        with open(f'curriculo/{projeto.nome}.pdf', 'wb') as pdf_novo:
+            pdf_novo.write(base64.decodebytes(bytes(projeto.curriculo, 'UTF8')))
+        projeto.foto = f"Curriculo do {projeto.nome}"
         self.projeto.create_projetos(projeto)
 
     def update(self, projeto, id):
         with open(f'imagens/{projeto.nome}.png', 'wb') as imagem_nova:
             imagem_nova.write(base64.decodebytes(bytes(projeto.foto, 'UTF8')))
         projeto.foto = f"Imagem do {projeto.nome}"
+        with open(f'curriculo/{projeto.nome}.pdf', 'wb') as pdf_novo:
+            pdf_novo.write(base64.decodebytes(bytes(projeto.curriculo, 'UTF8')))
+        projeto.foto = f"Curriculo do {projeto.nome}"
+
         self.projeto.update_projetos(projeto, id)
 
     def delete(self, id):
